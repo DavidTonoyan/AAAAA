437edcf85220b89931d33453cc65716b22760a28
David Tonoyan <davtonoyan1998@mail.ru>
2019-10-01 06:32:54 +0400

    String

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AAAAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            string myName,myNewName,myNew;
+          
+            myName=Console.ReadLine();
+            
+            for(int i=0;i<=myName.Length-1;i++)
+             {
+                 
+                 myNewName=Microsoft.VisualBasic.Strings.Mid(myName,myName.Length-i,1);
+                 myNew = myNewName;
+                 Console.Write(myNew);
+             }
+        
+        }
+    }
+}
+
+        
